a14e93df59e553f9da634a1e3684183d4b594e4c
lean1702 <leandrosandoval1702@gmail.com>
2020-12-10 20:19:22 -0300

    Importe de filtro

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { AngularEditorModule } from '@kolkov/angular-editor';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MatBadgeModule} from '@angular/material/badge';
 //terminar de ver video de delfor para importar el filtrador de provincia
+import { Ng2SearchPipeModule } from "ng2-search-filter";
+import { NgxPaginationModule } from "ngx-pagination";
 import { LayoutModule } from '@angular/cdk/layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
@@ -44,6 +46,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     MatBadgeModule,
     BrowserAnimationsModule,
     LayoutModule,
+    Ng2SearchPipeModule,
+    NgxPaginationModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
