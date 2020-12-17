1cdf5afce0d28250cfab6d0de366c04f65349982
lean1702 <leandrosandoval1702@gmail.com>
2020-12-17 15:51:26 -0300

    Designacion

--- backend/dist/controllers/soporte.controllers.js
+++ backend/dist/controllers/soporte.controllers.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.SoporteController = void 0;
 class SoporteController {
     static Soporte(Soporte) {
         throw new Error("Method not implemented.");
